380b6ab606785b3f9ea70b46ed277006c78da34d
iasandcb <iasandcb@gmail.com>
2014-09-23 23:27:28 +0900

    add menu in progress

---
+++ static/js/app.js
@@ -0,0 +1,30 @@
+var annApp = angular.module('annApp', ['ngMaterial', 'ngRoute', 'annControllers']);
+
+annApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/home', {
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+ }]);
+
+annApp.factory('menu', function() {
+    var sections = [
+    {
+        name: 'creations'
+    }
+    {
+        name: 'creators'
+    }
+    {
+        name: 'tags'
+    }
+    {
+        name: 'profile'
+    }
+    ];
+});
